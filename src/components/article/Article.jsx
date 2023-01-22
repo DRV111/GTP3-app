0ddfab1ad16c745e1c0d9f07e5589818df7cb025
DRV111 <xblackcrownx@gmail.com>
2023-01-22 17:56:04 +0300

    feat: add Blog section

--- src/components/article/Article.jsx
+++ src/components/article/Article.jsx
@@ -1,8 +1,21 @@
 import React from 'react';
 import './article.css';
 
-const Article = () => {
-  return <div>Article</div>;
+const Article = ({ imgUrl, date, title }) => {
+  return (
+    <div className="gpt3__blog-container_article">
+      <div className="gpt3__blog-container_article-image">
+        <img src={imgUrl} alt="blog img" />
+      </div>
+      <div className="gpt3__blog-container_article-content">
+        <div>
+          <h4>{date}</h4>
+          <h2>{title}</h2>
+        </div>
+        <p>Read Full Article</p>
+      </div>
+    </div>
+  );
 };
 
 export default Article;
